cf2874fda2c6ccd69ac7b666cd30049cbb0d9d61
kirakrishnan <aravindakrishnan92@gmail.com>
2019-05-07 22:32:25 -0400

    updating new fields in the db

--- database.py
+++ database.py
@@ -5,14 +5,17 @@ c = conn.cursor()
 
 # Create table in the database
 def users():
-    c.execute('CREATE TABLE IF NOT EXISTS users(userid TEXT, name TEXT, password TEXT, PRIMARY KEY (userid))')
+    c.execute('CREATE TABLE IF NOT EXISTS users(userid TEXT, name TEXT, password TEXT, country TEXT, phone INTEGER, location TEXT, PRIMARY KEY (userid))')
 
 # creates a sample records this is just for testing purpose we will remove it as we create a signup page
 # and create users from it
 def insert_record():
-    c.execute("insert INTO users VALUES ('admin','admin','admin')")
-    c.execute("insert INTO users VALUES ('admin1','admin1','admin1')")
+    c.execute("insert INTO users VALUES ('admin','admin','admin', 'USA', 12345678, '123abcd123')")
+    c.execute("insert INTO users VALUES ('admin1','admin1','admin1', 'INDIA', 90215678, '456abcd456')")
     conn.commit()
 
 users()
 insert_record()
+
+c.close()
+conn.close()
